a2f4b659ea13b0b102f0f33fbbf38ea6ed62b5f8
guofeng <3169887281@qq.com>
2018-06-06 16:21:51 +0800

    allow host

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#qqv(+g^h0%1^9roz=hm43w-*y3d_y!t#596gyxzm2ac8nepal'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.hithey.com']
 
 
 # Application definition
